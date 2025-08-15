ddf71f95a9bbb17233b8e46f639763071be4215a
marslan199 <mianarslan19900@gmail.com>
2025-08-15 11:09:02 -0700

    Adding_new_colum_for_making_machine_readable

--- Data_Manipulations.py
+++ Data_Manipulations.py
@@ -1,11 +1,25 @@
 import pandas as pd
+#
+# spam_df = pd.read_csv("spam_emails.csv")
+# ham_df = pd.read_csv("ham_emails.csv")
 
-spam_df = pd.read_csv("spam_emails.csv")
-ham_df = pd.read_csv("ham_emails.csv")
+# df = pd.concat([spam_df, ham_df], ignore_index=True)
 
-df = pd.concat([spam_df, ham_df], ignore_index=True)
+# # Save the merged file
+# df.to_csv("emails.csv", index=False)
 
-# Save the merged file
-df.to_csv("emails.csv", index=False)
+# print("Merged into emails.csv successfully.")
+# #
 
-print("Merged into emails.csv successfully.")
+# lets make data cleaning 
+df = pd.read_csv("emails.csv")
+# print(df.head())
+# print(df.isnull().sum())
+# print(df["label"].dtype)
+# print(df["id"].dtype)
+# print(df["email"].dtype)
+
+df["labelling"] = df["label"].map({"ham" : 0 , "spam" : 1})
+print(df.head())
+print(df.tail())
+
