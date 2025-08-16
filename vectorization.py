fb0b0ade9bda2bd9f154316e77db4e71d6cd04bc
marslan199 <mianarslan19900@gmail.com>
2025-08-16 10:56:16 -0700

    Data_Splitting

--- vectorization.py
+++ vectorization.py
@@ -1,8 +1,18 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd 
+from sklearn.model_selection import train_test_split
 vectorizer = TfidfVectorizer()
 df = pd.read_csv("emails.csv")
 
 X = vectorizer.fit_transform(df["clean_email"])
 # print(df.columns)
-print(X)
+# print(X)
+
+# lets start trainig
+y = df["labelling"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
+
+
